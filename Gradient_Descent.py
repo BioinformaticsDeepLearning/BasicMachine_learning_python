ee0358ae08d2b03aa158ff9c5331933ae4affd13
Geeky Olivia <49845542+BioinformaticsDeepLearning@users.noreply.github.com>
2020-09-29 09:53:03 +0200

    Update Gradient_Descent.py

--- Gradient_Descent.py
+++ Gradient_Descent.py
@@ -1,13 +1,17 @@
 #This is the Gradient descent code for optimization of any machine learning algorithm#
 
-#@created by Dr. Alisha parveen
+#@created by Dr.Alisha parveen
 
 import numpy as np
-def grad_desc(x,y):
+
+x =np.array([1,2,3,4,5])
+y= np.array([10,12,13,8,19])
+
+def gradient_descent(x,y):
     m_curr= b_curr= 0
-    iterations=10           #(change gradually)
+    iterations=10    #(change gradually like take 10, 100, 1000, 1200, 14000 and so on)
     n= len(x)
-    learning_rate = 0.05    #(change gradually)
+    learning_rate = 0.05   #(change gradually from 0.0001, 0.001, 0.01, 0.1, 0.005 and so on)
     
     for i in range(iterations):
         y_predicted = m_curr*x + b_curr
@@ -18,9 +22,6 @@ def grad_desc(x,y):
         b_curr= b_curr-learning_rate*d_b
         print("m {}, b {}, cost {}, iterations {}".format(m_curr, b_curr, cost, i))
 
-x =np.array([1,2,3,4,5])
-y= np.array([10,12,13,8,19])
-
-grad_desc(x,y)
+gradient_descent(x,y)
 
 #Change iterations and the learning_rate parameter to get the least value of cost/error#
